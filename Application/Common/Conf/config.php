20f1ff1146216de21b5e7c106a5048fc50f2a33c
宋宇航 <15835617681@163.com>
2017-04-14 10:19:07 +0800

    空项目

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'URL_ROUTER_ON '=>true,  
+    'URL_MODEL'     => 2, 
+    'VAR_PATHINFO'=> 's',
+	'DEFAULT_MODULE'            =>    'Home',  // 默认模块
+	// 'DEFAULT_CONTROLLER'    =>  'App',//默认控制器
+	// 'DEFAULT_ACTION'        =>  'index', // 默认操作名称
+	'DB_TYPE'               =>  'Mysql',     // 数据库类型
+	'DB_HOST'               =>  '182.92.157.126', // 服务器地址
+	'DB_NAME'               =>  'youlehai',          // 数据库名
+	'DB_USER'               =>  'root',      // 用户名
+	'DB_PWD'                =>  'root',          // 密码
+	'DB_PORT'               =>  '3306',        // 端口
+	'DB_PREFIX'             =>  'table_',    // 数据库表前缀
+);
